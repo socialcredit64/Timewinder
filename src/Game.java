19076d8844ba8344b501301994c35a390b239a83
socialcredit64 <rayontkkg@gmail.com>
2023-03-24 15:32:50 -0500

    init

---
+++ src/Game.java
@@ -0,0 +1,166 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage; 
+import java.awt.event.*; 
+import java.util.ArrayList;
+
+public class Game  extends JPanel implements Runnable, KeyListener, MouseListener, MouseMotionListener{
+
+	
+	private BufferedImage back; 
+	private int key; 
+	
+	public Game() {
+		new Thread(this).start();	
+		this.addKeyListener(this);
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		key =-1; 
+		
+	}
+
+	
+		
+		
+
+	
+	
+	public void run()
+	   {
+	   	try
+	   	{
+	   		while(true)
+	   		{
+	   		   Thread.currentThread().sleep(5);
+	            repaint();
+	         }
+	      }
+	   		catch(Exception e)
+	      {
+	      }
+	  	}
+	
+
+	
+	
+	
+	public void paint(Graphics g){
+		
+		Graphics2D twoDgraph = (Graphics2D) g; 
+		if( back ==null)
+			back=(BufferedImage)( (createImage(getWidth(), getHeight()))); 
+		
+
+		Graphics g2d = back.createGraphics(); 
+	
+		g2d.clearRect(0,0,getSize().width, getSize().height);
+		
+		g2d.setFont( new Font("SANS_SERIF", Font.BOLD, 50));
+		
+		g2d.drawString(String.valueOf(key)+" testing font", 400, 400);
+		
+		
+		
+		twoDgraph.drawImage(back, null, 0, 0);
+
+	}
+
+	
+	
+
+
+
+	//DO NOT DELETE
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+//DO NOT DELETE
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		key= e.getKeyCode();
+		System.out.println(key);
+		
+		
+		
+	
+	}
+	public void movement() {
+		
+	}
+
+	//DO NOT DELETE
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+
+	}
+
+
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	private static final int WIDTH =1800;
+	private static final int HEIGHT=1000;
+	
+	public Main () {
+		super("Timewinder");
+		setSize(WIDTH, HEIGHT);
+		Game play = new Game();
+		((Component) play).setFocusable(true);
+		
+		Color Dark = new Color(102, 102, 102);
+		
+		
+		setBackground(Dark);
+		
+		
+		getContentPane().add(play);
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		Main run = new Main();
+		
+
+	}
+
+
+}
+
+
