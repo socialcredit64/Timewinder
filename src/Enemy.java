3d9ff9cbaf81ea651e8f6368bc169954bbfabd48
socialcredit64 <124119894+socialcredit64@users.noreply.github.com>
2023-04-26 23:14:25 -0500

    Delete src directory

--- src/Enemy.java
+++
@@ -1,8 +0,0 @@
-public class Enemy extends Character{
-    public Enemy(){
-        super();
-    }
-    public Enemy(int xV, int yV, int health){
-        super(xV, yV, health);
-    }
-}
--- src/EnemyProj.java
+++
@@ -1,8 +0,0 @@
-public class EnemyProj {
-    public EnemyProj(){
-        super();
-    }
-    public Enemyproj(int xV, int yV, int damage){
-        super(xV,yV,damage);
-    }
-}
